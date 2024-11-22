20c0b028245b65a167390aabded0c2fd5e6f1c43
MrXgroot <161230118+MrXgroot@users.noreply.github.com>
2024-11-22 16:16:57 +0530

    added new features

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const io = new Server(server, {
 });
 const PORT = process.env.PORT || 5000;
 const games = {};
+const waitingQueue = [];
 
 io.on("connection", (socket) => {
   console.log("user connnected", socket.id);
@@ -27,8 +28,32 @@ io.on("connection", (socket) => {
       players: { white: socket.id, black: null },
     };
     socket.join(gameId);
+    socket.gameId = gameId;
     socket.emit("gameCreated", { gameId, role: "white" }); //can change later
     console.log(`Game created: ${gameId}`);
+  });
+
+  socket.on("joinRandom", () => {
+    if (waitingQueue.length !== 0) {
+      const gameId = `game-${Date.now()}`;
+
+      const player = socket;
+      const opponent = waitingQueue.shift();
+      games[gameId] = {
+        chess: new Chess(),
+        players: { white: player, black: opponent },
+      };
+      player.join(gameId);
+      opponent.join(gameId);
+      player.gameId = gameId;
+      opponent.gameId = gameId;
+      player.emit("gameCreated", { gameId, role: "white" });
+      opponent.emit("gameCreated", { gameId, role: "black" });
+      io.to(gameId).emit("loadBoard");
+    } else {
+      waitingQueue.push(socket);
+      socket.emit("waitnigForOpponent");
+    }
   });
 
   socket.on("joinGame", (gameId) => {
@@ -36,6 +61,8 @@ io.on("connection", (socket) => {
     if (game && !game.players.black) {
       game.players.black = socket.id;
       socket.join(gameId);
+      socket.gameId = gameId;
+      console.log("jointed", socket.gameId);
       socket.emit("playerJoined", { players: game.players, gameId });
       socket.emit("roleAssigned", { role: "black" });
       io.to(gameId).emit("loadBoard");
@@ -49,6 +76,7 @@ io.on("connection", (socket) => {
       socket.emit("error", "Game not Found");
       return;
     }
+
     let result;
     try {
       result = game.chess.move(move);
@@ -58,6 +86,10 @@ io.on("connection", (socket) => {
       // socket.emit('error','Invalid move');
       return;
     }
+    if (game.chess.isGameOver()) {
+      console.log("check mate");
+      io.to(gameId).emit("matchClosed", "Check Mate..!");
+    }
     io.to(gameId).emit("moveMade", {
       board: game.chess.board(),
       fen: game.chess.fen(),
@@ -66,7 +98,22 @@ io.on("connection", (socket) => {
   });
 
   //  socket gets disconnected
-  socket.on("disconnect", () => console.log("disconnectded"));
+  socket.on("disconnect", () => {
+    console.log("player Disconnected");
+    if (socket.gameId) {
+      delete games[socket.gameId];
+      console.log("game deleted");
+      console.log("socket.gameId", socket.gameId);
+      io.to(socket.gameId).emit("matchClosed", "Opponent left the game");
+    }
+
+    //remove the waiting queue
+    const index = waitingQueue.indexOf(socket);
+    if (index !== -1) {
+      waitingQueue.splice(index, 1);
+      console.log(`Player ${socket.id} removed from waiting queue`);
+    }
+  });
 });
 
 server.listen(PORT, () => {
